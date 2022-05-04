2d5b287cb8f3139fe9be4c19d715202f2c37e725
em-edu <81432745+em-edu@users.noreply.github.com>
2022-05-04 08:15:46 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,21 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+
+const mainTL = gsap.timeline();
+
+mainTL.from("#rect",{duration:1, drawSVG:"50% 50%"})
+.from("#rect",{duration:1, fill:"#fff"}, "-=75%")
+.to("#rect",{duration:1, fill:"green"})
+.to("#rect",{duration:1, morphSVG:"#tria", fill:"#FF0000"})
+.to("#rect",{duration:1, morphSVG:{shape:"#star", shapeIndex: 0}, fill:"#0060EF"})
+
+;
+
+
+
+
+
+
